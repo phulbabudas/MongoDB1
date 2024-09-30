e8684151e7c3fbd15db76d9d0be33ccd7820e02a
phulbabudas <phulbabudas123@gmeil.com>
2024-10-01 03:40:53 +0530

    connect moongodb to mongoose

---
+++ OneDrive/Desktop/crud/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser";
+import dotenv  from "dotenv";
+const app = express();
+app .use (bodyParser.json());
+
+
+
+
+
+app.use(bodyParser.json());
+dotenv.config();
+const PORT = process.env.PORT||7000;
+const MONGOURL =process.env.MONGO_URL;
+
+mongoose
+.connect (MONGOURL)
+.then(()=> {
+console.log("Database connected successful.");
+app.listem(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`)
+   });
+})
+.catch((error)=>console.log(error));
