4163693d528f82c35c95189d0c8c8ba392118373
phulbabudas <phulbabudas123@gmeil.com>
2024-10-01 21:38:02 +0530

    add to mongodb and mongoose

--- OneDrive/Desktop/crud/index.js
+++ OneDrive/Desktop/crud/index.js
@@ -3,22 +3,17 @@ import mongoose from "mongoose"
 import bodyParser from "body-parser";
 import dotenv  from "dotenv";
 const app = express();
-app .use (bodyParser.json());
-
-
-
-
-
-app.use(bodyParser.json());
+app.use (bodyParser.json());
 dotenv.config();
 const PORT = process.env.PORT||7000;
 const MONGOURL =process.env.MONGO_URL;
-
+console.log(PORT)
+console.log(MONGOURL)
 mongoose
 .connect (MONGOURL)
 .then(()=> {
 console.log("Database connected successful.");
-app.listem(PORT,()=>{
+app.listen(PORT,()=>{
   console.log(`Server is running on port ${PORT}`)
    });
 })
